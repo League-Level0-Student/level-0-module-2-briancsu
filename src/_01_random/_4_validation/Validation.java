a0e66cdcaaaf4b88bfbd1c0e8fc4e0b6ee4b48e7
league <league@imac-14.attlocal.net>
2022-12-02 16:58:48 -0800

    I went to module 2!

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -11,15 +11,33 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
+		for (int i = 0; i < 10; i ++) {
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
-
+		
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber == 0) {
+			System.out.println("hi");
+		}
+		else if (randomNumber == 1) {
+			System.out.println("hallo");
+		}
+		else if (randomNumber == 2) {
+			System.out.println("helo");
+		}
+		else if (randomNumber == 3) {
+			System.out.println("you are cool");
+		}
+		else {
+			System.out.println("you are smart");
+		}		
+		
+		
+		
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
+}
 }
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,22 +6,37 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
-
+	public static void main(String[] args) {
 	// 2. Get the user to enter a question for the 8 ball to answer
-	
+	JOptionPane.showInputDialog(null, "Ask a question: ");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
-
+	Random ran = new Random();
 	
 	// 4. If the random number is 0
-
+	int num = ran.nextInt(3-0 + 1) +1;
+	System.out.println(num);
 	// -- tell the user "Yes"
-
+	if (num == 0) {
+		System.out.println("yes");
+	}
+	else if (num == 1) {
+		System.out.println("no");
+	}
+	else if (num == 2) {
+		System.out.println("ask google");
+	}
+	else if (num == 3) {
+		System.out.println("ask yourself");
+	}
+	
 	// 5. If the random number is 1
 
 	// -- tell the user "No"
@@ -34,4 +49,5 @@ public class Magic8Ball {
 
 	// -- write your own answer
 
+}
 }
