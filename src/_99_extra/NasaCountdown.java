5f06598a29ae156cb955a6a1dd40dc735a22ecab
league <league@imac-14.attlocal.net>
2022-12-16 16:57:09 -0800

    Only a few more lessons and I'll be done!

--- src/_99_extra/NasaCountdown.java
+++ src/_99_extra/NasaCountdown.java
@@ -1,6 +1,10 @@
 
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
+import game_tools.Sound;
+
 /* NASA wants to shoot a rocket into orbit.
  * Somebody has to do the countdown or the rocket won't launch.
  * 
@@ -11,19 +15,28 @@ package _99_extra;
  **/
 
 public class NasaCountdown {
-    public static void main(String[] args) throws InterruptedException {
-        // 2. Use a pop-up to ask the user where to start counting from
-        
-        // 3. Change the countdown to use the new starting point
-        
-        // 1. Print a countdown from 10 to 0 on the console
-            
-            // 4. Use the Sound.speak method to hear the countdown.
-            
-            // 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
-            
-        // 5. when the counting is done, speak "blastoff!"
-    }
+	public static void main(String[] args) throws InterruptedException {
+		// 2. Use a pop-up to ask the user where to start counting from
+		String countdown = JOptionPane.showInputDialog(null, "Where to start counting down from: ");
+		// 3. Change the countdown to use the new starting point
+		int countint = Integer.parseInt(countdown);
+		// 1. Print a countdown from 10 to 0 on the console
+		while(countint >= 0) {    
+			Thread.sleep(1000);
+			System.out.println(countint);
+			countint -= 1;
+			String s = countint+"";
+			Sound.speak(s);
+			
+			if (countint == 0){
+				Sound.speak("Blastoff!");
+			}
+			// 4. Use the Sound.speak method to hear the countdown.
+		} 
+		// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
+
+		// 5. when the counting is done, speak "blastoff!"
+	}
 }
 
 
--- src/_99_extra/StarShow.java
+++ src/_99_extra/StarShow.java
@@ -14,40 +14,42 @@ public class StarShow {
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+			//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
-
+		robot.setSpeed(8);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+		int robX  = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 600
-
+		int robY = 600;
 		// 8. Make a variable to hold the star size and set it to 25
-
+		int starSize = 25;
 		// 12. Repeat the steps #19 to #18, 30 times
 
 			// 19. Set the pen width to i 
-	
+			for (int i = 0; i < 30; i++) {
+			robot.setPenWidth(i);
 			// 10. Set the X position of the robot to your X variable
-	
+			robot.setX(robX);
 			// 11. Set the Y position of the robot to your Y variable
-	
+			robot.setY(robY);
 			// 9. Call the drawStar() method with your star size variable
-	
+			drawStar(starSize);
 			// 14. Increase the X position by star size. See Figure 2.
-	
+			robX += starSize;
 			// 15. decrease the Y position by star size. See Figure 3.
-	
+			robY -= starSize;
 			// 16. Increase the star size by 20
-	
+			starSize += 20;
 			// 17. Turn the robot 12 degrees
-	
+			robot.turn(12);
+			robot.setRandomPenColor();
 			// 18. Make each star a different random color like in Figure 4. 
-
+			}
 	}
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+		robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
 
 			// 1. Move the robot the distance of the starSize variable
